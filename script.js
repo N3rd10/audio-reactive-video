628b417faed6e9245dbe61c85f0d3364ce33046e
N3rd <landonmarting@icloud.com>
2024-12-20 12:58:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service worker.js')
         .then((registration) => {
             console.log('Service Worker registered with scope:', registration.scope);
@@ -6,7 +6,7 @@ if ('serviceWorker' in navigator) {
         .catch((error) => {
             console.error('Service Worker registration failed:', error);
         });
-}
+}*/
 
 let audioContext;
 let analyser;
